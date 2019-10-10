37de82d895731ca4984786d61116944412ca3370
wangpeng <wangpeng@sinovine.com>
2019-10-10 18:18:01 +0800

    页面

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,13 @@
  * Date: 2019/10/10
  * Time: 17:02
  */
+
+/*
+ * 此方法会将当前请求的路由名称转换为 CSS 类名称，
+ * 作用是允许我们针对某个页面做页面样式定制。
+ * 在后面的章节中会用到。
+ * */
+function route_class()
+{
+    return str_replace('.','-',Route::currentRouteName());
+}
