408c6e67c21ca59a777e1e77f3d59b6a27ff70a9
HH88023 <wp_0357@163.com>
2019-10-27 22:45:31 +0800

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -12,6 +12,7 @@ class TopicObserver
     //在Topic模型保存时触发saving事件中，对excerpt字段进行赋值
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
     }
 
